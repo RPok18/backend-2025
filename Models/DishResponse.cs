983c01931b66d0e668014b6b050ab7415e115e0a
Rishabh Pokhriyal <155913097+RPok18@users.noreply.github.com>
2025-09-21 21:36:57 +0700

    Add files via upload
    two new models for respective job

---
+++ Models/DishResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Dishapi.Models
+{
+    public class DishResponse
+    {
+        public List<Dish> Dishes { get; set; } = new();
+        public Pagination Pagination { get; set; } = new();
+    }
+}
---
+++ Models/pagination.cs
@@ -0,0 +1,9 @@
+﻿namespace Dishapi.Models
+{
+    public class Pagination
+    {
+        public int Size { get; set; }
+        public int Count { get; set; }
+        public int Current { get; set; }
+    }
+}
